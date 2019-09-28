92e71cb24a974682a8afdc75c61088a03ba21025
lpile <logan.pile@gmail.com>
2019-09-28 16:40:28 -0600

    Add devise User model with migration, route, and skeleton specs

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable, :lockable
+end
