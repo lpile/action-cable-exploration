0e601340994cef2cfe8694362336f4006e51083e
lpile <logan.pile@gmail.com>
2019-09-28 13:13:32 -0600

    Add DatabaseCleaner gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,7 +40,19 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
+
+  # DatabaseCleaner config
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 
   # factory_bot_rails config
   config.include FactoryBot::Syntax::Methods
